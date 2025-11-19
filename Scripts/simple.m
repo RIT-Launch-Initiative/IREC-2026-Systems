249cc0aad8cb186feea75fcf80defb32d6a14880
cowsed <richie.sommers@outlook.com>
2025-11-19 17:46:57 -0500

    make the script do things

--- Scripts/simple.m
+++ Scripts/simple.m
@@ -1,1 +1,5 @@
 fprintf("Hello from github actions %f", 1234.5678)
+fprintf(1, "Hello from github actions to 1 %f", 1234.5678)
+fprintf(2, "Hello from github actions to 1 %f", 1234.5678)
+
+disp(1234)
